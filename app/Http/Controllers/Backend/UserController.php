b85a7a343e6a2c482cad1003675ecf03fdb69bfd
DiiKhanh <duykhanh.030803@gmail.com>
2023-09-07 00:12:51 +0700

    first commit

---
+++ app/Http/Controllers/Backend/UserController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers\Backend;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    public function index(){
+        $template = 'backend.dashboard.user.index';
+        return view('backend.dashboard.layout', compact('template'));
+    }
+}
---
+++ resources/views/backend/dashboard/component/footer.blade.php
@@ -0,0 +1,8 @@
+<div class="footer">
+    <div class="pull-right">
+      <strong>DiiKhanh</strong>
+    </div>
+    <div>
+      <strong>Copyright</strong> Example Company &copy; 2023
+    </div>
+</div>
